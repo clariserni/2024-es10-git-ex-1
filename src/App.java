96bfb2b273a9762885889576c3cf17fca986acd0
clariserni <clarissaserni.96@gmail.com>
2024-06-25 12:59:09 +0200

    prova

--- src/App.java
+++ src/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        testMain();
+    }
+
+    public static void testMain(){
         System.out.println("Hello, World!");
     }
 }
